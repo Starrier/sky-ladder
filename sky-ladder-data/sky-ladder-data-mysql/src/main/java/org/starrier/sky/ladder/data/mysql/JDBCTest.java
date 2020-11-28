bf4cfc548222ac328926bc8e3175678189e4d04a
starrier <starrier@starrier.org>
2020-11-28 17:31:12 +0800

    add new modules to record

---
+++ sky-ladder-data/sky-ladder-data-mysql/src/main/java/org/starrier/sky/ladder/data/mysql/JDBCTest.java
@@ -0,0 +1,10 @@
+package org.starrier.sky.ladder.data.mysql;
+
+/**
+ * @author starrier
+ * @date 2020/11/28
+ */
+public class JDBCTest {
+
+
+}
---
+++ sky-ladder-data/sky-ladder-data-mysql/src/main/java/org/starrier/sky/ladder/data/mysql/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * MySQL Learning
+ *
+ * @author starrier
+ * @date 2020/11/28
+ */
+package org.starrier.sky.ladder.data.mysql;
