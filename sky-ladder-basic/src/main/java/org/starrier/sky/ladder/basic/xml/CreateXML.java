53ffb3f779baf0c03cc05407a0fa25d025a657dd
starrier <starrier@starrier.org>
2020-11-30 21:57:47 +0800

    add xml

---
+++ sky-ladder-basic/src/main/java/org/starrier/sky/ladder/basic/xml/CreateXML.java
@@ -0,0 +1,158 @@
+package org.starrier.sky.ladder.basic.xml;
+
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.XMLWriter;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+
+import static org.starrier.sky.ladder.basic.xml.ReadXML.readXmlDocument;
+
+/**
+ * @author starrier
+ * @date 2020/11/30
+ */
+public class CreateXML {
+
+    private static final Logger logger = LoggerFactory.getLogger(CreateXML.class);
+
+    public boolean createUserDotXML(String path, String type) {
+
+        boolean writen = true;
+        OutputStream outputStream = null;
+        XMLWriter xmlWriter = null;
+        Document document = null;
+
+        String rootPath = path;
+
+        try {
+            document = DocumentHelper.createDocument();
+            Element newRootNode = DocumentHelper.createElement(type);
+            Element newModule1Node = DocumentHelper.createElement("module");
+
+            newModule1Node.addAttribute("id", "1");
+            newRootNode.add(newModule1Node);
+            document.add(newRootNode);
+
+            OutputFormat outputFormat = new OutputFormat();
+            outputFormat.setEncoding("UTF-8");
+
+            outputStream = new FileOutputStream(rootPath);
+            xmlWriter = new XMLWriter(outputStream, outputFormat);
+
+            xmlWriter.write(document);
+
+        } catch (IOException e) {
+            logger.error("ConfigFileServiceImpl.createServerDotXML error: " + e);
+            return false;
+        } catch (Exception e) {
+            logger.error("ConfigFileServiceImpl.createServerDotXML error: " + e);
+            return false;
+        }
+
+        return writen;
+    }
+
+    public boolean writeUserDotXML(String path, String module_id, String userName, String passWord) {
+
+        boolean writen = true;
+
+        XMLWriter xmlWriter = null;
+        Document document;
+
+        String rootPath = path;
+
+        try (OutputStream outputStream = new FileOutputStream(rootPath);) {
+            document = readXmlDocument(rootPath);
+            if (document != null) {
+                Element root = document.getRootElement();
+                List<Element> elementList = root.elements();
+
+                for (Element ele : elementList) {
+
+                    String id = ele.attributeValue("id");
+                    if (id.equals(module_id)) {
+
+                        List<Element> userList = ele.elements();
+
+                        if (userList != null && userList.size() > 0) {
+
+                            // 进行比对，是否已存在数据
+                            for (Element user : userList) {
+
+                                Element name = user.element("name");
+                                String nameValue = name.getText();
+
+                                if (nameValue.equals(userName)) {
+                                    writen = false;
+                                    break;
+                                }
+                            }
+
+                            // 有需求才新增
+                            if (writen) {
+
+                                Element new_user_node = DocumentHelper.createElement("user");
+                                Element new_name_node = DocumentHelper.createElement("name");
+                                Element new_pass_node = DocumentHelper.createElement("password");
+                                Element new_date_node = DocumentHelper.createElement("date");
+
+                                new_name_node.setText(userName);
+                                new_pass_node.setText(passWord);
+
+                                new_user_node.addAttribute("index", userList.size() + 1 + "");
+
+                                new_user_node.add(new_name_node);
+                                new_user_node.add(new_pass_node);
+                                new_user_node.add(new_date_node);
+
+                                ele.add(new_user_node);
+                            }
+                        } else {
+
+                            Element new_user_node = DocumentHelper.createElement("user");
+                            Element new_name_node = DocumentHelper.createElement("name");
+                            Element new_pass_node = DocumentHelper.createElement("password");
+                            Element new_date_node = DocumentHelper.createElement("date");
+
+                            new_name_node.setText(userName);
+                            new_pass_node.setText(passWord);
+
+                            new_user_node.addAttribute("index", "1");
+
+                            new_user_node.add(new_name_node);
+                            new_user_node.add(new_pass_node);
+                            new_user_node.add(new_date_node);
+
+                            ele.add(new_user_node);
+                        }
+                        break;
+                    }
+                }
+            }
+
+            if (writen) {
+                OutputFormat outputFormat = new OutputFormat();
+                outputFormat.setEncoding("UTF-8");
+
+
+                xmlWriter = new XMLWriter(outputStream, outputFormat);
+
+                xmlWriter.write(document);
+            }
+        } catch (IOException e) {
+            logger.error("XMLFileServiceImpl.writeChangeDotXML error: " + e);
+        } catch (Exception e) {
+            logger.error("XMLFileServiceImpl.writeChangeDotXML error: " + e);
+        }
+
+        return writen;
+    }
+}
---
+++ sky-ladder-basic/src/main/java/org/starrier/sky/ladder/basic/xml/ReadXML.java
@@ -0,0 +1,107 @@
+package org.starrier.sky.ladder.basic.xml;
+
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+import jdk.internal.util.xml.impl.Input;
+import org.apache.pdfbox.util.XMLUtil;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author starrier
+ * @date 2020/11/30
+ */
+public class ReadXML {
+
+    private static final Logger logger = LoggerFactory.getLogger(ReadXML.class);
+
+    public static Document readXmlDocument(String filePath){
+
+
+        Document doc = null;
+
+        // 解析xml文档内容
+        try(InputStream  in = new FileInputStream(new File(filePath))) {
+            SAXReader reader = new SAXReader();
+            //in = XMLUtil.class.getClassLoader().getResourceAsStream(filePath);// 获取到xml文件
+
+            doc = reader.read(in);
+        } catch (Exception e) {
+            logger.error("XMLUtil.readXml error: "+ e);
+            return null;
+        }
+        return doc;
+    }
+
+    public static List<Element> readXml(String filePath){
+
+
+        List<Element> elementList = null;
+        // 解析xml文档内容
+        try(InputStream in = new FileInputStream(new File(filePath))) {
+            SAXReader reader = new SAXReader();
+            //in = XMLUtil.class.getClassLoader().getResourceAsStream(filePath);// 获取到xml文件
+
+            Document doc = reader.read(in);
+
+            Element root = doc.getRootElement();
+            elementList = root.elements();
+            logger.debug("XMLUtil.readXml root name:" + root.getName());
+        } catch (Exception e) {
+            logger.error("XMLUtil.readXml error: "+ e);
+            return null;
+        }
+        return elementList;
+    }
+
+    public List<HashMap<String, String>> readUserDotXML(String path,String module_id) {
+
+        List<HashMap<String, String>> users = new ArrayList<HashMap<String, String>>();
+
+        String rootPath =  path;
+
+        List<Element> list = readXml(rootPath);
+        if (list != null) {
+            for (Element ele : list) {
+
+                String index = ele.attributeValue("id");
+
+                if (module_id.equals(index)) {
+
+                    List<Element> userList = ele.elements();
+
+                    if (userList != null && userList.size() > 0) {
+                        for (Element user : userList) {
+
+                            HashMap<String, String> hashMap = new HashMap<String, String>();
+
+                            Element name = user.element("name");
+                            Element password = user.element("password");
+
+                            String nameValue = name.getText();
+                            String passwordValue = password.getText();
+
+                            hashMap.put("name", nameValue);
+                            hashMap.put("password", passwordValue);
+
+                            users.add(hashMap);
+                        }
+                    }
+
+                    logger.debug("XMLFileServiceImpl.readUserDotXML Element module_id:" + module_id + ",users size:" + users.size());
+                    break;
+                }
+            }
+        }
+
+        return users;
+    }
+}
