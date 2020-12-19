9336671ae28b24bcf8466e1cb05f5843fe8ea97e
starrier <starrier@starrier.org>
2020-12-19 15:57:24 +0800

    add new module for springboot basic

---
+++ sky-ladder-distribution/sky-ladder-distribution-springboot/src/main/java/org/starrier/sky/ladder/distribution/spring/boot/SpringBootApplication.java
@@ -0,0 +1,15 @@
+package org.starrier.sky.ladder.distribution.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @author starrier
+ * @date 2020/12/19
+ */
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplication.class, args);
+    }
+}
